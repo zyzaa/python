45f051d394a93e8882e001af44e9a4acae35290e
ZYZ <2692318203@qq.com>
2021-02-03 19:18:05 +0800

    第一天 学生管理系统

--- StudentManagerSystem/Students.py
+++
@@ -1,8 +0,0 @@
-class Student(object):
-    def __init__(self, id, name, sex):
-        self.id = id
-        self.name = name
-        self.sex = sex
-
-    def __str__(self):
-        return f"{self.id},{self.name},{self.sex}"
--- StudentManagerSystem/main.py
+++
@@ -1,5 +0,0 @@
-from managerSystem import *
-
-if __name__ == '__main__':
-    m1 = ManagerSystem()
-    m1.run()
